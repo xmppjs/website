7ccfd4a2b9bf6fc0b7c20c370cdaa3d0deabaef1
Sonny Piers <sonny@fastmail.net>
2018-02-25 23:48:57 +0100

    bump dependencies

--- components/Header.js
+++ components/Header.js
@@ -12,8 +12,8 @@ const Header = ({title = '', pathname}) => (
 			<link rel="icon" type="image/png" href="/static/favicon.png"/>
 			<link href="/static/bootstrap.min.css" rel="stylesheet" type="text/css"/>
 			<link href="/static/prism.css" rel="stylesheet" type="text/css"/>
-			<script src="/static/jquery.slim.min.js"/>
-			<script src="/static/bootstrap.min.js"/>
+			{/* <script src="/static/jquery.slim.min.js"/> */}
+			{/* <script src="/static/bootstrap.min.js"/> */}
 			<script src="/static/prism.js"/>
 		</Head>
 		<Navigation pathname={pathname}/>
